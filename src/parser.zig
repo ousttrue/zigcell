ea24f644a41a8dd5dd4c4f3818bb1cad3578436f
ousttrue <ousttrue@gmail.com>
2022-07-11 21:23:29 +0900

    traverse

--- src/parser.zig
+++ src/parser.zig
@@ -1,6 +1,27 @@
 const std = @import("std");
 const zls = @import("zls");
 
+const indent_buffer = " " ** 1024;
+
+fn indent(level: usize) []const u8 {
+    return indent_buffer[0..level];
+}
+
+fn traverse(tree: std.zig.Ast, node_idx: u32, level: u32) void {
+    const tags = tree.nodes.items(.tag);
+    const node_tag = tags[node_idx];
+    std.debug.print("[{d:0>3}]{s}{s}\n", .{ node_idx, indent(level), @tagName(node_tag) });
+
+    // children
+    if (zls.ast.isContainer(tree, node_idx)) {
+        var buf: [2]std.zig.Ast.Node.Index = undefined;
+        const ast_decls = zls.ast.declMembers(tree, node_idx, &buf);
+        for (ast_decls) |decl| {
+            traverse(tree, decl, level + 1);
+        }
+    }
+}
+
 pub const Parser = struct {
     const Self = @This();
 
@@ -25,8 +46,9 @@ pub const Parser = struct {
         defer allocator.free(text);
         const tree: std.zig.Ast = try std.zig.parse(allocator, text);
 
-        const scope = try zls.analysis.makeDocumentScope(allocator, tree);
-        scope.debugPrint();
+        // const scope = try zls.analysis.makeDocumentScope(allocator, tree);
+        // scope.debugPrint();
+        traverse(tree, 0, 0);
 
         errdefer tree.deinit(allocator);
         return Self.new(allocator, tree);
