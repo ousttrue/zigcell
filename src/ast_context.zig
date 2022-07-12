9342a1e7937e17f784152f013360a22130515e6a
ousttrue <ousttrue@gmail.com>
2022-07-12 20:26:00 +0900

    AstContext

---
+++ src/ast_context.zig
@@ -0,0 +1,57 @@
+const std = @import("std");
+
+fn getAllTokens(allocator: std.mem.Allocator, source: [:0]const u8) std.ArrayList(std.zig.Token) {
+    var tokens = std.ArrayList(std.zig.Token).init(allocator);
+    var tokenizer: std.zig.Tokenizer = .{
+        .buffer = source,
+        .index = 0,
+        .pending_invalid_token = null,
+    };
+
+    while (true) {
+        const token = tokenizer.next();
+        if (token.tag == .eof) {
+            break;
+        }
+        tokens.append(token) catch unreachable;
+    }
+
+    return tokens;
+}
+
+pub const AstContext = struct {
+    const Self = @This();
+    allocator: std.mem.Allocator,
+    text: [:0]const u8,
+    tree: std.zig.Ast,
+    tokens: std.ArrayList(std.zig.Token),
+
+    pub fn new(allocator: std.mem.Allocator, src: []const u16) *Self {
+        const text = std.unicode.utf16leToUtf8AllocZ(allocator, src) catch unreachable;
+        const tree: std.zig.Ast = std.zig.parse(allocator, text) catch unreachable;
+
+        var self = allocator.create(Self) catch unreachable;
+        self.* = Self{
+            .allocator = allocator,
+            .text = text,
+            .tree = tree,
+            .tokens = getAllTokens(allocator, text),
+        };
+        return self;
+    }
+
+    pub fn delete(self: Self) void {
+        self.tokens.deinit();
+        self.tree.deinit();
+        self.allocator.free(self.text);
+        self.allocator.destroy(self);
+    }
+
+    fn getTokens(self: Self, start: usize, last: usize) []const std.zig.Token {
+        var end = last;
+        if (end < self.tokens.items.len) {
+            end += 1;
+        }
+        return self.tokens.items[start..end];
+    }
+};
--- src/parser.zig
+++ src/parser.zig
@@ -1,78 +1,39 @@
 const std = @import("std");
 const Node = @import("./node.zig").Node;
-
-fn getAllTokens(allocator: std.mem.Allocator, source: [:0]const u8) std.ArrayList(std.zig.Token) {
-    var tokens = std.ArrayList(std.zig.Token).init(allocator);
-    var tokenizer: std.zig.Tokenizer = .{
-        .buffer = source,
-        .index = 0,
-        .pending_invalid_token = null,
-    };
-
-    while (true) {
-        const token = tokenizer.next();
-        if (token.tag == .eof) {
-            break;
-        }
-        tokens.append(token) catch unreachable;
-    }
-
-    return tokens;
-}
+const AstContext = @import("./ast_context.zig").AstContext;
 
 pub const Parser = struct {
     const Self = @This();
 
-    allocator: std.mem.Allocator,
-    tree: std.zig.Ast,
-    tokens: std.ArrayList(std.zig.Token),
+    context: *AstContext,
     nodes: std.ArrayList(Node),
-    node_stack: std.ArrayList(u32),
 
-    pub fn new(allocator: std.mem.Allocator, tree: std.zig.Ast) *Self {
+    pub fn new(allocator: std.mem.Allocator, context: *AstContext) *Self {
         var self = allocator.create(Self) catch unreachable;
         self.* = Self{
-            .allocator = allocator,
-            .tree = tree,
-            .tokens = getAllTokens(allocator, tree.source),
+            .context = context,
             .nodes = std.ArrayList(Node).init(allocator),
-            .node_stack = std.ArrayList(u32).init(allocator),
         };
-
         return self;
     }
 
     pub fn delete(self: Self) void {
-        self.tokens.deinit();
-        self.node_stack.deinit(self.allocator);
+        self.nodes.deinit();
         self.allocator.destroy(self);
     }
 
     pub fn parse(allocator: std.mem.Allocator, src: []const u16) !*Self {
-        const text = try std.unicode.utf16leToUtf8AllocZ(allocator, src);
-        defer allocator.free(text);
-        const tree: std.zig.Ast = try std.zig.parse(allocator, text);
+        const context = AstContext.new(allocator, src);
 
-        errdefer tree.deinit(allocator);
-        var self = Self.new(allocator, tree);
+        var self = Self.new(allocator, context);
 
-        self.traverse();
-
-        return self;
-    }
-
-    fn traverse(self: *Self) void {
-        for (self.tree.rootDecls()) |decl| {
-            Node.init(0, &self.tree, decl).traverse(0);
+        for (context.tree.rootDecls()) |decl| {
+            var node = Node.init(0, &context.tree, decl);
+            node.traverse(0);
+            self.nodes.append(node) catch unreachable;
         }
         std.debug.print("\n", .{});
-    }
 
-    fn getTokens(self: Self, start: usize, last: usize) []const std.zig.Token {
-        var end = last;
-        if (end < self.tokens.items.len) {
-            end += 1;
-        }
-        return self.tokens.items[start..end];
+        return self;
     }
 };
